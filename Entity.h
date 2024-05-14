2bf350c135fa80657fbbdb2ebab95ac7b62bb7dd
MAterrwr <empororhotdowg21@empororhotdowg2>
2024-05-14 14:52:45 +0930

    entity done

---
+++ Entity.h
@@ -0,0 +1,19 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+class Entity{
+    protected:
+    int health;
+    int attack;
+    int defense;
+    public:
+    Entity();
+    Entity(int health, int attack, int defense);
+    int getHealth();
+    int getAttack();
+    int getDefense();
+    void setHealth(int health);
+    void setAttack(int attack);
+    void setDefense(int defense);
+};
+#endif 

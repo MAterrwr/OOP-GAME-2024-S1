c22b2738739aecdb4234a4e5b55bdd062fd33da8
William Entwistle <williamentwistle@Williams-MBP-2.modem>
2024-05-20 16:39:50 +0930

    inventory update 1

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,17 +1,37 @@
 #include "Inventory.h"
+#include <iostream>
+
+using namespace std;
 
 Inventory::Inventory() : max_items(10), item_count(0), trophy("") {
-    item_names = string[max_items];
-    item_quantities = int[max_items];
+    item_names = new string[max_items];
+    item_quantities = new int[max_items];
 
-    add_item("Health Potion", 1); 
-    add_item("Skill Potion", 1); 
+    string health_potion = "Health Potion";
+    string skill_potion = "Skill Potion";
+    add_item(health_potion, 1);
+    add_item(skill_potion, 1); 
 }
 
-
 Inventory::~Inventory() {
-    delete [] item_names;
-    delete [] item_quantities;
+    delete[] item_names;
+    delete[] item_quantities;
+}
+
+void Inventory::add_item(string& item_name, int quantity) {
+    for (int i = 0; i < item_count; ++i) {
+        if (item_names[i] == item_name) {
+            item_quantities[i] += quantity;
+            return;
+        }
+    }
+    if (item_count < max_items) {
+        item_names[item_count] = item_name;
+        item_quantities[item_count] = quantity;
+        item_count++;
+    } else {
+        cout << "Inventory is full!" << endl;
+    }
 }
 
 void Inventory::use_item(string& item_name) {
@@ -19,8 +39,7 @@ void Inventory::use_item(string& item_name) {
         if (item_names[i] == item_name) {
             if (item_quantities[i] > 0) {
                 item_quantities[i]--;
-                cout << "Used one " << item_name 
-                << ". Remaining: " << item_quantities[i] << endl;
+                cout << "Used one " << item_name << ". Remaining: " << item_quantities[i] << endl;
                 if (item_quantities[i] == 0) {
                     for (int j = i; j < item_count - 1; ++j) {
                         item_names[j] = item_names[j + 1];
@@ -37,8 +56,8 @@ void Inventory::use_item(string& item_name) {
     cout << "Item " << item_name << " not found!" << endl;
 }
 
-void Inventory::print_inventory()  {
-    cout << "Inventory:" << std::endl;
+void Inventory::print_inventory() {
+    cout << "Inventory:" << endl;
     for (int i = 0; i < item_count; ++i) {
         cout << item_names[i] << ": " << item_quantities[i] << endl;
     }
@@ -47,10 +66,10 @@ void Inventory::print_inventory()  {
     }
 }
 
-void Inventory::set_trophy( string& trophy_name) {
+void Inventory::set_trophy(string& trophy_name) {
     trophy = trophy_name;
 }
 
-string Inventory::get_trophy()  {
+string Inventory::get_trophy() {
     return trophy;
 }
--- Inventory.h
+++ Inventory.h
@@ -1,25 +1,28 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
-#include <string> 
-#include <iostream>
+
+#include <string>
 #include "Player.h"
+
 using namespace std;
 
-class Inventory {
-    private:
-    int max_items;
-    string* item_names;
-    int* item_quantities;
+class Inventory : public Player {
+private:
+    int max_items; 
+    string* item_names; 
+    int* item_quantities; 
     int item_count;
     string trophy;
 
-    public:
+public:
     Inventory();
     ~Inventory();
+
+    void add_item(string& item_name, int quantity);
     void use_item(string& item_name);
     void print_inventory();
     void set_trophy(string& trophy_name);
     string get_trophy();
 };
 
-#endif
+#endif 

88c378b2f2d3e80333d3ecc943a5bfb541a024e1
root <root@WillEntwistle>
2024-05-23 00:12:01 +0930

    jerry

--- Monster.cpp
+++ Monster.cpp
@@ -2,33 +2,34 @@
 
 using namespace std;
 
+//
 Monster::Monster() {
-    health = 0;
+    health = 0; //initialise stats to 0
     damage = 0;
     defense = 0;
     level = 0;
 }
 
 Monster::Monster(int health, int damage, int defense, int level) {
-    this->health = health * level;
+    this->health = health * level; //set the health damage and defense based on level
     this->damage = damage * level;
     this->defense = defense * level;
     this->level = level;
 }
 
 void Monster::takeDamage(int Damage) {
-    health -= Damage;
+    health -= Damage; //subtract the damage from the health
 
     if (health < 0) {
-        health = 0;
+        health = 0; //ensure health does not go below 0
     }
 }
 
 bool Monster::isAlive() {
     if (health > 0) {
-        return true;
+        return true; //monster is alive
     } else {
-        return false;
+        return false; //monster is dead
     }
 } 
 
@@ -41,5 +42,5 @@ void Monster::setLevel(int level) {
 }
 
 void Monster::print() {
-    cout << "Craeting monster ..." << endl;
+    cout << "Creating monster ..." << endl;
 }
--- Monster.h
+++ Monster.h
@@ -6,17 +6,17 @@
 
 class Monster : public Entity {
 protected:
-    int level;
+    int level; //Level of the monster, affects the health, daamage and defence
 public:
-    Monster();
-    Monster(int health, int damage, int defense, int level);
-    void takeDamage(int Damage);
-    bool isAlive();
+    Monster(); //Default constructor
+    Monster(int health, int damage, int defense, int level); //Constructor
+    void takeDamage(int Damage); //Method to reduce the monsters health by taking damage
+    bool isAlive(); //Method to check if monster is still alive
 
-    int getLevel();
-    void setLevel(int level);
+    int getLevel(); //Getter for the monsters level
+    void setLevel(int level); //Setter for the monsters level
 
-    void print();
+    void print(); //Method to print the monsters details
 };
 
 #endif
